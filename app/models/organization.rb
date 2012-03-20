86e72ebd5e6b7698aba9c06faf7142a3f6e7df01
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-03-20 12:48:44 +0200

    Clean up associations, create them if needed

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,7 +3,5 @@ class Organization
 
   field :name, type: String
 
-  has_many    :organs
-  has_many    :children, class_name: "Organization"
   belongs_to  :parent, class_name: "Organization"
 end
