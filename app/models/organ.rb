cf4669c3b7e9e98b1e8038fa8b9e7496fda6558c
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-03-21 16:49:58 +0200

    Added more Organ fields

--- app/models/organ.rb
+++ app/models/organ.rb
@@ -2,6 +2,10 @@ class Organ
   include Mongoid::Document
 
   field :name, localize: true
+  field :description, localize: true
+  field :term_start, type: Date
+  field :term_end, type: Date
+  field :owner, type: String # TODO: ref to Person
 
   belongs_to :organization
 end
