4451255f62820244804403cd89654eb1e1e9ca65
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2012-03-21 17:31:15 +0200

    init locale for every request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  protected
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
