a9f26308e40ab06bab91bbe05f33b5acdec761d9
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-03-22 16:30:00 +0200

    re-organize factory organizations

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -2,20 +2,38 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :spartan_teknillinen_yliopisto, :class => Organization do
-    name  'Spartan Teknillinen Yliopisto'
-    _id   '4f6b1edf91bc2b33d3000001'
+
+  factory :organization do
+    factory :spartan_teknillinen_yliopisto do
+      name  'Spartan Teknillinen Yliopisto'
+      _id   '4f6b1edf91bc2b33d3010000'
+    end
+
+    factory :luonnontieteellinen_tiedekunta do
+      name  'Luonnontieteellinen tiedekunta'
+      _id   '4f6b1edf91bc2b33d3010100'
+      parent Factory(:spartan_teknillinen_yliopisto)
+    end
+
+    factory :kirjasto do
+      name  'Kirjasto'
+      _id   '4f6b1edf91bc2b33d3010200'
+      parent Factory(:spartan_teknillinen_yliopisto)
+    end
+
+    factory :kemian_laitos do
+      name  'Kemian laitos'
+      _id   '4f6b1edf91bc2b33d3010101'
+      parent  Factory(:luonnontieteellinen_tiedekunta)
+    end
+
+    factory :fysiikan_laitos do
+      name  'Fysiikan laitos'
+      _id   '4f6b1edf91bc2b33d3010102'
+      parent  Factory(:luonnontieteellinen_tiedekunta)
+    end
   end
 
-  factory :luonnontieteellinen_tiedekunta, :class => Organization do
-    name  'Luonnontieteellinen tiedekunta'
-    _id   '4f6b1edf91bc2b33d3000002'
-    parent Factory(:spartan_teknillinen_yliopisto)
-  end
 
-  factory :kemian_laitos, :class => Organization do
-    name  'Kemian laitos'
-    _id   '4f6b1edf91bc2b33d3000003'
-    parent  Factory(:luonnontieteellinen_tiedekunta)
-  end
+
 end
