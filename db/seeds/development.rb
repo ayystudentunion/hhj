1fe137f86f1701a9b541bae1980d5eea15c0fa90
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-03-22 22:49:38 +0200

    Create Organs data for development env

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -16,34 +17,44 @@ end
 createOrganization( {
   name: 'Spartan Teknillinen Yliopisto',
   _id:  '4f6b1edf91bc2b33d3010000',
-  children: [
+  children: [ {
+    name: 'Luonnontieteellinen tiedekunta',
+    _id:   '4f6b1edf91bc2b33d3010100',
+    children: [
     {
-      name: 'Luonnontieteellinen tiedekunta',
-      _id:   '4f6b1edf91bc2b33d3010100',
-      children: [
-      {
-        name: 'Kemian laitos',
-        _id:   '4f6b1edf91bc2b33d3010101'
-      }, {
-        name: 'Fysiikan laitos',
-        _id:   '4f6b1edf91bc2b33d3010102'
-      } ]
+      name: 'Kemian laitos',
+      _id:   '4f6b1edf91bc2b33d3010101'
     }, {
-      name: 'Humanististen tieteiden tiedekunta',
-      _id:   '4f6b1edf91bc2b33d3010300',
-      children: [
-      {
-        name: 'Filosofinen laitos',
-        _id:   '4f6b1edf91bc2b33d3010301'
-      }, {
-        name: 'Naistutkimuksen laitos',
-        _id:   '4f6b1edf91bc2b33d3010302'
-      } ]
+      name: 'Fysiikan laitos',
+      _id:   '4f6b1edf91bc2b33d3010102'
+    } ]
+  }, {
+    name: 'Humanististen tieteiden tiedekunta',
+    _id:   '4f6b1edf91bc2b33d3010300',
+    children: [
+    {
+      name: 'Filosofinen laitos',
+      _id:   '4f6b1edf91bc2b33d3010301'
     }, {
-      name: 'Kirjasto',
-      _id:   '4f6b1edf91bc2b33d3010200'
-    }
-  ]
+      name: 'Naistutkimuksen laitos',
+      _id:   '4f6b1edf91bc2b33d3010302'
+    } ]
+  }, {
+    name: 'Kirjasto',
+    _id:   '4f6b1edf91bc2b33d3010200'
+  } ]
 } )
 
+def createOrgans(organs)
+   organs.each{ |organ| Factory(:organ, organ) }
+end
 
+createOrgans( [ {
+  name:  'Tasa-arvotyöryhmä',
+  _id:   '4f6b1edf91bc2b3301010101',
+  organization: Organization.find('4f6b1edf91bc2b33d3010101')
+}, {
+  name:  'Kirjakerho',
+  _id:   '4f6b1edf91bc2b3302010101',
+  organization: Organization.find('4f6b1edf91bc2b33d3010200')
+} ] )
