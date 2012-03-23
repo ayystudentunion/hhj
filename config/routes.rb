fc5361399bbc0887df01871aea166ba28eab2d60
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2012-03-23 16:54:44 +0200

    added name to index route

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Halloped::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  match ':locale' => 'pages#index'
+  match ':locale' => 'pages#index', :as => :index
   root :to => 'pages#locale_redirect'
 
   # See how all your routes lay out with "rake routes"
