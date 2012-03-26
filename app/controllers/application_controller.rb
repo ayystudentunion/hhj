8d9f02a551a5ec2af784e5bb1c1eabd01e1ffc09
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-03-26 14:59:32 +0300

    Railsified organization selector to create organ form. Use Mongoid::Tree extension for parent - child mapping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :set_locale
+  before_filter :set_university
 
   protected
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+
+  def set_university
+    @university = Organization.root
+  end
+
 end
