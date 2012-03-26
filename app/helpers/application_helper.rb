8ccad8e2724c8a8326487929ddf1f7b89c00176e
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2012-03-26 13:55:04 +0300

    refactoring modal form routing and rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,29 @@ module ApplicationHelper
     I18n.fallbacks.keys.concat(I18n.available_locales).uniq
   end
 
+  def modal_dialog
+    case params[:modal]
+    when 'new-organ'
+      render "modals/create-new-organ"
+    when 'cfa'
+      render "modals/call-for-application"
+    when 'application'
+      render "modals/received-application"
+    when 'message'
+      render "modals/send-group-message"
+    when 'send-application'
+      render "modals/send-application"
+    else
+      ;
+    end
+  end
+
+  def modal_wrap_style
+    if params[:modal]
+      ""
+    else
+      "display: none"
+    end
+  end
+
 end
