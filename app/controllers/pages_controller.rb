7af6437ea5076eaca95d0e7e4a07f6ee298ddf93
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2012-03-27 15:22:57 +0300

    fragment support for index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,10 @@ require 'factory_girl_rails'
 class PagesController < ApplicationController
 
   def index
-    @organ = Factory.build(:organ)
-
-    render "pages/_frontpage"
+    respond_to do |format|
+      format.html { render "pages/_frontpage" }
+      format.fragment { render "pages/_frontpage.html", layout: false }
+    end
   end
 
   def locale_redirect
