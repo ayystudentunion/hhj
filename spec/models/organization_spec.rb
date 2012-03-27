9e2e306dbc2b9bd37ef8cb618b0b0fc4dd9a6c01
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-03-27 14:10:01 +0300

    Support for getting full hash of tree with recursive children

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe Organization do
 
+  it 'can be converted to full tree hash with all recursive children' do
+    Factory(:kirjasto)
+    tree = Organization.root.tree_hash[:children]
+    tree.count.should > 0
+    str_dump = tree.to_s
+    str_dump.should include('Kirjasto')
+  end
+
 end
