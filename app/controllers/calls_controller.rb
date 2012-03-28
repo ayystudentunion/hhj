d1ee4af475a5b90f9c7c81083dad5a3a6325faaf
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2012-03-28 14:58:50 +0300

    implementing call-for-applications controller

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -11,4 +11,32 @@ class CallsController < ApplicationController
     end
   end
 
+  def create # create a new organ document
+
+    organ = Factory(:call, params[:call_for_applications])
+
+    respond_to do |format|
+      format.json { render json: organ.to_json }
+      format.html { redirect_to action: :show, id: organ._id }
+    end
+  end
+
+  def show # return a single organ
+    @call = CallForApplications.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render :json => @call }
+      format.fragment { render "show", formats: ['html'], layout: false }
+    end
+  end
+
+  def edit # form for modifing an existing organ
+  end
+
+  def update # modify an existing organ
+  end
+
+  def destroy # delete an organ
+  end
+
 end
