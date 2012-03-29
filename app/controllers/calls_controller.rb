1bc1865a970941d9cb4d09d40d60a8633ad1bc4d
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-03-29 15:16:59 +0300

    Assign organ (relation) to empty call

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -8,6 +8,7 @@ class CallsController < ApplicationController
   def new
     organ = Organ.find params[:organ_id]
     call  = Call.new #Factory(:call)
+    call.organ = organ
     respond_to do |format|
       format.fragment { render partial: "modals/call-for-application.html", locals: {organ: organ, call: call} }
     end
