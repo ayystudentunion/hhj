1075514e222669c5238a32ece23ab5ace359306b
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-03-30 11:02:52 +0300

    Localize the required Call fields

--- app/models/call.rb
+++ app/models/call.rb
@@ -5,17 +5,17 @@ class Call
 
   belongs_to :organ
 
-  field :title, type: String
+  field :title, localize: true
   field :member_amount, type: Integer
   field :deputy_amount, type: Integer
-  field :quota_information, type: String
+  field :quota_information, localize: true
   field :date_open, type: Date
   field :date_start, type: Date
   field :date_end, type: Date
   field :term_start, type: Date
   field :term_end, type: Date
-  field :appointment_place_and_date, type: String
-  field :description, type: String
+  field :appointment_place_and_date, localize: true
+  field :description, localize: true
 
   def formatted_term
     if term_start and term_end
