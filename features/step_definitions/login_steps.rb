66798d2a13afbc11961739a6a6175d51ada5ade8
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-02 11:29:20 +0300

    clean up test code

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,9 +1,5 @@
-def check_profile_info_contains(value)
-  find('.profile-info').should have_content value
-end
-
-def check_login_info_contains(value)
-  find('.login-info').should have_content value
+def check_profile_contains(selector, *values)
+  values.each{ |value| find(selector).should have_content value }
 end
 
 Given %r/^I am logged in as a student union employee$/ do
@@ -11,10 +7,8 @@ Given %r/^I am logged in as a student union employee$/ do
 end
 
 Then %r/^I should see logged in user "([^"]*)" with mail "([^"]*)" and phone "([^"]*)"$/ do |name, email, phone|
-  check_profile_info_contains name
-  check_profile_info_contains email
-  check_profile_info_contains phone
-  check_login_info_contains name
+  check_profile_contains '.profile-info', name, phone, email
+  check_profile_contains '.login-info', name
 end
 
 
