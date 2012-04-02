8f310670c499feb1a21a66c9ed81e902d21c6cce
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-02 10:52:26 +0300

    Remove unnecessary class specification from factory

--- spec/factories/organs.rb
+++ spec/factories/organs.rb
@@ -3,12 +3,12 @@
 
 FactoryGirl.define do
   factory :organ do
-    factory :tasa_arvotyoryhma, :class => Organ do
+    factory :tasa_arvotyoryhma do
       name  'Tasa-arvotyöryhmä'
       _id   "4f6b1edf91bc2b3301010101"
       association :organization, :factory => :kemian_laitos
     end
-    factory :kirjakerho, :class => Organ do
+    factory :kirjakerho do
       name  'Kirjakerho'
       _id   "4f6b1edf91bc2b3302010101"
       association :organization, :factory => :kirjasto
