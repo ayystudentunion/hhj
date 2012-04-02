b11880fabd983349c310278a0c0b72841d53284d
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-02 14:45:18 +0300

    Railsified the rest of the Position Application form fields

--- app/models/position_application.rb
+++ app/models/position_application.rb
@@ -1,6 +1,12 @@
 class PositionApplication
   include Mongoid::Document
 
+  validates :position, presence: true, :format => { :with => /(position_member|position_deputy|position_both)/ }
+
+  field :position, type: Symbol
+  field :deputy_of, type: String
+  field :personal_statement, type: String
+
   belongs_to :call
 
 end
