bceb0bb44da2aab3325929c4b6d1f89501ae574c
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-03 13:39:26 +0300

    Modified the call view step to work with only the name of call also

--- features/step_definitions/call_steps.rb
+++ features/step_definitions/call_steps.rb
@@ -1,9 +1,16 @@
-Then %r/^I should see call for application '([^']*)' with description '([^']*)':$/ do |call_name, description, table|
+Then %r/^I should see call for application '([^']*)'(?: with description '([^']*)':)?$/ do |call_name, description, table|
   check_details '.call-details', call_name, description, table
 end
 
+def check_personal_details(person_attributes)
+  check_that_contains_values '.applicant-details', person_attributes[:first_name], person_attributes[:last_name],
+    person_attributes[:phone], person_attributes[:email]
+end
+
 Then %r/^I should see my own name, phone number and email\-address in the application form$/ do
-  student_attributes = FactoryGirl.attributes_for(:eija)
-  check_that_contains_values '.applicant-details', student_attributes[:first_name], student_attributes[:last_name],
-    student_attributes[:phone], student_attributes[:email]
+  check_personal_details FactoryGirl.attributes_for(:eija)
+end
+
+Then %r/^I should see my own name, phone number and email\-address in the confirmation dialog$/ do
+  check_personal_details FactoryGirl.attributes_for(:eija)
 end
--- features/step_definitions/view_steps.rb
+++ features/step_definitions/view_steps.rb
@@ -1,13 +1,25 @@
+
+def check_view_values(container, table)
+  table.hashes.each do |row|
+    container.find(".label:contains('#{row[:label]}') + span").should have_content row[:value]
+  end
+end
+
 def check_details(details_selector, title, description, table)
   details = page.find details_selector
   details.find("h2").should have_content title
-  details.should have_content description
-
-  table.hashes.each do |row|
-    details.find(".label:contains('#{row[:label]}') + span").should have_content row[:value]
+  if description and table
+    details.should have_content description
+    check_view_values details, table
   end
 end
 
 def check_that_contains_values(container_selector, *values)
   values.each{ |value| find(container_selector).should have_content value }
+end
+
+Then %r/^I should see dialog '([^']*)':$/ do |title, table|
+  content = find(".modal .content")
+  content.find("h2:contains('#{title}")
+  check_view_values content, table
 end
