4b17038d8af836fdce20f01b22421fc560b51708
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-04 22:25:28 +0300

    Finished spec tests for setting the position selection results to applications

--- spec/models/call_spec.rb
+++ spec/models/call_spec.rb
@@ -4,15 +4,14 @@ require 'spec_helper'
 
 describe Call do
 
+  application = FactoryGirl.create(:kirjakerho_application)
+  call = application.call
+  call.position_applications.each do | application |
+    application.selected_as.should == nil
+  end
+
   it 'can handle empty hash as results to position applications' do
-    application = FactoryGirl.create(:kirjakerho_application)
-    call = application.call
-    call.position_applications.each do | application |
-      application.selected_as.should == nil
-    end
-
     call.set_results({})
-
     call.reload_relations
     call.position_applications.each do | application |
       application.selected_as.should == nil
@@ -20,20 +19,28 @@ describe Call do
 
   end
 
-  it 'can set results to position applications' do
-    application = FactoryGirl.create(:kirjakerho_application)
-    call = application.call
-    call.position_applications.each do | application |
-      application.selected_as.should == nil
-    end
-
+  it 'can set results to one position applications' do
     call.set_results position_deputy: call.position_applications.map(&:_id)
-
     call.reload_relations
     call.position_applications.each do | application |
-      puts application.selected_as
       application.selected_as.should == :position_deputy
     end
-
   end
+
+  it 'can set results to multiple position applications' do
+    applications = FactoryGirl.create_list(:kirjakerho_application, 7)
+
+    call.set_results(
+      position_deputy: applications[0..2].map(&:_id),
+      position_member: applications[3..4].map(&:_id),
+      position_rejected: [ applications[5]._id ],
+    )
+
+    call.reload_relations
+    call.position_applications.where(selected_as: :position_deputy).count.should == 3
+    call.position_applications.where(selected_as: :position_member).count.should == 2
+    call.position_applications.where(selected_as: :position_rejected).count.should == 1
+    call.position_applications.where(selected_as: nil).count.should == 1 # Don't change applications whose id is not in set_results args
+  end
+
 end
