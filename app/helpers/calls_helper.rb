d379b08cb17e28c8a302f2b610f6de9e81d8e530
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-04 10:36:03 +0300

    Added helper function to format "looking for" statement

--- app/helpers/calls_helper.rb
+++ app/helpers/calls_helper.rb
@@ -1,2 +1,5 @@
 module CallsHelper
+  def formatted_looking_for(call)
+    t('calls.show.members', :count => call.member_amount) + ", " + t("calls.show.deputies", count: call.deputy_amount)
+  end
 end
