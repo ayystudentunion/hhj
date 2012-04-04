9636215f2e398ec6844a0a5f68b0110e9e6dbb3f
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-04 15:25:54 +0300

    fix "DEPRECATION WARNING: Factory.create is deprecated; use FactoryGirl.create instead"

--- spec/models/organ_spec.rb
+++ spec/models/organ_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe Organ do
   it 'can use factory girl...' do
-    organ = Factory.create(:tasa_arvotyoryhma)
+    organ = FactoryGirl.create(:tasa_arvotyoryhma)
     organ.name.should  == 'Tasa-arvotyöryhmä'
     organ.organization.name.should  == 'Kemian laitos'
     organ.organization.parent.parent.name.should == 'Spartan Teknillinen Yliopisto'
