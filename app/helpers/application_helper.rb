1ff4e4d8a58218c7e8b3e6f5ec7ac1161184185a
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-05 13:13:29 +0300

    Do not show combined terms in member card, instead just show all instances of same member with their terms.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,12 @@ module ApplicationHelper
     Halloped::languages
   end
 
+  def formatted_term_of(member)
+    term = formatted_date member.term_start, end_date: member.term_end
+    term = t 'organs.member.open_ended' if term.blank?
+    term
+  end
+
   def formatted_date(date, options={})
     [date, options[:end_date]].reject{ |date|
       date.blank? and options[:end_date].nil?
