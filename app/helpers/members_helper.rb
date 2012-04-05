9599842ecc8abc182c9d376f97175bd3f38c7c74
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-05 13:00:33 +0300

    Show the terms of member in organ

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,10 @@
 module MembersHelper
+  def formatted_terms(member)
+    same_members = member.organ.members.where(user_id: member.user._id).asc(:term_end)
+    same_members.map{|member|
+      term = formatted_date member.term_start, end_date:member.term_end
+      term = t 'organs.member.open_ended' if term.blank?
+      term
+    }.join(', ')
+  end
 end
