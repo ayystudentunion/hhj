b7e9205e8f8ed338a7f1a193ffbbeb9dfbcf92da
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-05 12:59:15 +0300

    Improved date_formatter to output - date or date - if there is start_date or end_date is nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,20 @@ module ApplicationHelper
   end
 
   def formatted_date(date, options={})
-    [date, options[:end_date]].reject(&:blank?).map{|date| I18n.l(date, format: options[:format])}.join(' - ')
+    [date, options[:end_date]].reject{ |date|
+      date.blank? and options[:end_date].nil?
+    }.map{ |date|
+      format_date date, options
+    }.join(' - ')
   end
 
+  protected
+
+  def format_date(date, options)
+    if date.nil?
+        ''
+    else
+      I18n.l(date, format: options[:format])
+    end
+  end
 end
