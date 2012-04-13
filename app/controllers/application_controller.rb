1c8a210192043af8aea2f6c2ce0d62af256314de
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-13 11:56:22 +0300

    Only root organizations are accepted as universities in routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,14 +14,16 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    locale = params[:locale] || I18n.default_locale
+    return if params[:locale].nil?
+    locale = params[:locale]
     redirect_to(root_path) unless Halloped::languages.include?(locale.to_sym)
     I18n.locale = locale
   end
 
   def set_university
-    @university = Organization.where(key: params[:university]).first
-    redirect_to(index_path locale: params[:locale]) if @university.nil?
+    return if params[:university].nil?
+    @university = Organization.roots.where(key: params[:university]).first
+    redirect_to(index_path(locale: params[:locale])) if @university.nil?
   end
 
 end
