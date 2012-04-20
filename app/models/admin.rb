958b4e9e16640984c7efb7954cbdba36f9a462d0
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-20 14:49:02 +0300

    Disabled "reset password" functionality.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
   field :email,              :type => String, :null => false, :default => ""
