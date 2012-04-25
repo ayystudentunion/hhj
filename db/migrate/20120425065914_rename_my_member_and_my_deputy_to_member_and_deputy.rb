9db38de8f2f1240fbad159b437381fcbe6126ca2
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-25 14:01:00 +0300

    Fix migration to clear old unused attributes

--- db/migrate/20120425065914_rename_my_member_and_my_deputy_to_member_and_deputy.rb
+++ db/migrate/20120425065914_rename_my_member_and_my_deputy_to_member_and_deputy.rb
@@ -1,16 +1,20 @@
 class RenameMyMemberAndMyDeputyToMemberAndDeputy < Mongoid::Migration
   def self.up
     PositionApplication.all.each do |a|
-      a.member = a.my_member
-      a.deputy = a.my_deputy
+      a[:member_id] = a[:my_member_id] if a[:my_member_id]
+      a[:deputy_id] = a[:my_deputy_id] if a[:my_deputy_id]
+      a.remove_attribute(:my_member_id)
+      a.remove_attribute(:my_deputy_id)
       a.save
     end
   end
 
   def self.down
     PositionApplication.all.each do |a|
-      a.my_member = a.member
-      a.my_deputy = a.deputy
+      a[:my_member_id] = a[:member_id] if a[:member_id]
+      a[:my_deputy_id] = a[:deputy_id] if a[:deputy_id]
+      a.remove_attribute(:member_id)
+      a.remove_attribute(:deputy_id)
       a.save
     end
   end
