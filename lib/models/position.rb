bfd5eab3558357e9407a668c848f35f194495d56
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2012-04-30 14:55:29 +0300

    Use the custom position symbol name also in Position methods.

--- lib/models/position.rb
+++ lib/models/position.rb
@@ -10,10 +10,11 @@ module Models
 
       class_eval do
         def self.position_field(symbol)
-          validates symbol, allow_nil: true, inclusion: { in:  POSITION_VALUES }
-          field symbol, type: Symbol
-          scope :current_members, where(symbol => :position_member, current: true)
-          scope :current_deputies, where(symbol => :position_deputy, current: true)
+          @@position_field_symbol = symbol
+          validates @@position_field_symbol, allow_nil: true, inclusion: { in:  POSITION_VALUES }
+          field @@position_field_symbol, type: Symbol
+          scope :current_members, where(@@position_field_symbol => :position_member, current: true)
+          scope :current_deputies, where(@@position_field_symbol => :position_deputy, current: true)
         end
       end
     end
@@ -23,8 +24,8 @@ module Models
     end
 
     def set_position!(params)
-      return to_not_selected! if params[:position].nil?
-      case params[:position].to_sym
+      return to_not_selected! if params[@@position_field_symbol].nil?
+      case params[@@position_field_symbol].to_sym
         when :position_member then to_member! params[:deputy]
         when :position_deputy then to_deputy! params[:member]
       end
@@ -43,38 +44,38 @@ module Models
     end
 
     def to_member!(deputy_id)
-      self.selected_as = :position_member
+      self[@@position_field_symbol] = :position_member
       self.member = nil
       self.deputy = with_deputy_and_member_relations_cleared(deputy_id)
       save!
     end
 
     def to_deputy!(member_id)
-      self.selected_as = :position_deputy
+      self[@@position_field_symbol] = :position_deputy
       self.deputy = nil
       self.member = with_deputy_and_member_relations_cleared(member_id)
       save!
     end
 
     def to_not_selected!
-      self.selected_as = nil
+      self[@@position_field_symbol] = nil
       self.member = nil
       self.deputy = nil
       save!
     end
 
     def validate_member_and_deputy_positions
-      def has_value(relation, allowed_position)
+      def has_position(relation, allowed_position)
         other = send(relation)
         return if other.nil?
-        if other.selected_as != allowed_position
+        if other[@@position_field_symbol] != allowed_position
           errors.add(
               relation, errors.generate_message(
-                relation, :position))
+                relation, @@position_field_symbol))
         end
       end
-      has_value :deputy, :position_deputy
-      has_value :member, :position_member
+      has_position :deputy, :position_deputy
+      has_position :member, :position_member
     end
   end
 end
