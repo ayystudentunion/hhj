704efd31698a725e1cfec7d90b9402fd62e5f7e8
Janne Peltola <janne.j.peltola@gmail.com>
2012-11-19 17:34:08 +0200

    Added some UTF-8 encoding mojo

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Encoding.default_internal = 'UTF-8'
+Encoding.default_external = 'UTF-8'
+
 # Initialize the rails application
 Halloped::Application.initialize!
