51c851bbdc2066c21f31c9667014915fbf0ff622
Janne Peltola <janne.j.peltola@gmail.com>
2012-11-23 19:51:04 +0200

    Typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,6 @@ Halloped::Application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'admin@halloped.fi'}
 
   # Enable threaded mode
   # config.threadsafe!
