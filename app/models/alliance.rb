312680422d9ecd304ac38596bdeec58abe1c3af6
Satu Salekari <satu.salekari@experq.com>
2013-06-18 16:54:23 +0300

    Validation for checking that all applications belong to the selected call

--- app/models/alliance.rb
+++ app/models/alliance.rb
@@ -9,8 +9,16 @@ class Alliance
 
   validates_presence_of :creator
 
+  validate :applications_must_belong_to_the_call
+
   def members
     self.position_applications.map{|application| application.user}
+  end
+
+  def applications_must_belong_to_the_call
+    self.position_applications.each do |application|
+       errors.add(:call, :does_not_match_applications) unless application.call == self.call
+    end
   end
 
 end
