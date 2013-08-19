a5594880d66e059ebe4f256f5ac453d87c78c109
Satu Salekari <satu.salekari@experq.com>
2013-08-19 10:17:09 +0300

    Seed data updated
    Now doesn't have two open calls for same organ.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -16,6 +16,7 @@ Rake::Task["db:import:university"].execute
 
 helsinki = Organization.university_by_key('helsinki')
 helsinki.update_attributes!(alliances_enabled: true,recommendations_threshold: 2)
+
 hallitus = helsinki.organs.create!(name: "Hallitus", official: true)
 professori = helsinki.users.create!(first_name: "Toimi", last_name: "Sumelius", email: "toimi@helsinki.fi")
 hallitus.members.create!(user: professori, group: :group_professors)
@@ -26,15 +27,16 @@ member = hallitus.members.create!(user: member_user, group: :group_hallopeds, po
 deputy_user = helsinki.users.create!(first_name: "David", last_name: "Kummala", email: "david.kummala@helsinki.fi")
 deputy = hallitus.members.create!(user: deputy_user, group: :group_hallopeds, position: :position_deputy, member: member, term_start: Time.utc(2012,2,3), term_end: Time.utc(2014,2,3))
 
-hallitus_call = hallitus.calls.create!(title: "Opiskelijaedustajia hallitukseen!", member_amount: 2, deputy_amount: 1, workflow: :workflow_proposal_available)
+administrative_call  = hallitus.calls.create!(title: "Hallintovaali", member_amount: 2, deputy_amount: 1, workflow:  :workflow_administration_election)
 student_pekka = helsinki.users.create!(first_name: "Pekka", last_name: "Lemminkäinen", email: "pekka.lemminkainen@helsinki.fi")
 student_paula = helsinki.users.create!(first_name: "Paula", last_name: "Rimminki", email: "paula.rimminki@helsinki.fi")
-pekka_application = hallitus_call.position_applications.create(user: student_pekka, position: :position_member, personal_statement: "Olisin hyvä halloped!")
-hallitus_call.position_applications.create(user: student_paula, position: :position_deputy, member: pekka_application, personal_statement: "Haluaisin vain varajäseneksi siis.")
+pekka_application = administrative_call.position_applications.create!(user: student_pekka, position: :position_member, personal_statement: "Olisin hyvä halloped!", custom: {"degree" => "YTM"} )
+administrative_call.position_applications.create!(user: student_paula, position: :position_deputy, member: pekka_application, personal_statement: "Haluaisin vain varajäseneksi siis.", custom: {"degree" => "HTM"})
 
-administrative_call  = hallitus.calls.create!(title: "Hallintovaali", member_amount: 2, deputy_amount: 1, workflow:  :workflow_administration_election)
-pekka_application = administrative_call.position_applications.create(user: student_pekka, position: :position_member, personal_statement: "Olisin hyvä halloped!")
-administrative_call.position_applications.create(user: student_paula, position: :position_deputy, member: pekka_application, personal_statement: "Haluaisin vain varajäseneksi siis.")
+recretional_group = helsinki.organs.create!(name: "Huvitoimikunta", official: false)
+recretional_group_call = recretional_group.calls.create!(title: "Opiskelijaedustajia huvitoimikuntaan!", member_amount: 2, deputy_amount: 1, workflow: :workflow_proposal_available)
+pekka_application = recretional_group_call.position_applications.create(user: student_pekka, position: :position_member, personal_statement: "Olisin hyvä huvittelija!")
+recretional_group_call.position_applications.create(user: student_paula, position: :position_deputy, member: pekka_application, personal_statement: "Haluaisin vain varajäseneksi siis.")
 
 kansanterveystiede = helsinki.descendants.where(name: "Kansanterveystiede").first
 tasaarvotyoryhma = kansanterveystiede.organs.create!(name: "Tasa-arvotyöryhmä", official: true)
