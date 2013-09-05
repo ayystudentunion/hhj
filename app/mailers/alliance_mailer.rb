6fcc4f1f47d6cc94d2b90d2e0e562b9ebbe527ce
Satu Salekari <satu.salekari@experq.com>
2013-09-05 15:43:46 +0300

    Mention alliance creator in the notification email

--- app/mailers/alliance_mailer.rb
+++ app/mailers/alliance_mailer.rb
@@ -4,7 +4,7 @@ class AllianceMailer < ActionMailer::Base
   
   def confirmation_email(membership, university, url)
     @call_title = membership.position_application.call.title
-    @alliance_name = membership.alliance.name
+    @alliance = membership.alliance
     @university = university
     @url = url
     mail(:to => membership.position_application.user.email, :subject => "Sinut on lisätty vaaliliittoon. Samma på svenska. You have been added to an electoral alliance.")
