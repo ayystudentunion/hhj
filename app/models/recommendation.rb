ee2bdb040aa87f79e9b21f64ebb422593031742e
Satu Salekari <satu.salekari@experq.com>
2013-10-21 11:14:54 +0300

    Db index to prevent duplicates

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -7,4 +7,6 @@ class Recommendation
   validates_presence_of :position_application
   validates_presence_of :user
 
+  index({ position_application: 1, user: 1 }, { unique: true, drop_dups: true })
+
 end
