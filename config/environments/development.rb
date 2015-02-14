dde19807a94f98625b222320435f63fa4fc3a79a
Baiqua Fan <fan@experq.local>
2015-02-14 15:32:42 +0200

    Remove deprecated option 'config.whiny_nils = true' from environments: development.rb and test.rb.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Halloped::Application.configure do
   config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  # config.whiny_nils = true # Rails 4.0
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
