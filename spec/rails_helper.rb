8d0736739954e6a7d5add7222da777924187e8af
Baiqua Fan <fan@experq.local>
2015-02-17 15:40:22 +0200

    Fix 'factory_girl' error: 'factory not registered factorygirl' for Rails 4.0.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'spec_helper'
 require 'rspec/rails'
 #require 'rspec/autorun'
-require 'factory_girl'
+#require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
