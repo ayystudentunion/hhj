3e8125971f31c368458087f1f9e86c9bcab02122
Baiquan Fan <baiquan.fan@experq.com>
2015-02-22 04:49:41 +0200

    Modify application.rb to load lib tasks, etc., and to set time_zone and locale.

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,18 @@ module Halloped
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Helsinki'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('public/universities/**/locales/*.{yml}').to_s]
+    config.i18n.default_locale = :fi
   end
 end
