d167df8d55cf3c814c1450cb981ec9f4ec90aa37
Baiquan Fan <baiquan.fan@experq.com>
2015-02-27 13:02:11 +0200

    Add '*.css' to precompile additional assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.paths << Rails.root.join("public", "universities
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( *.png *.gif *.map *.yml *.slim )
+Rails.application.config.assets.precompile += %w( *.css *.png *.gif *.map *.yml *.slim )
