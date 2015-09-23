19f8fb1734730358b25805d2376a89c75274afe3
Petteri Räty <petsku@petteriraty.eu>
2015-09-23 11:48:05 +0300

    Fix deprecation warnings

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
 
   it 'can edit call results' do
-    user = Factory.create :aaro
+    user = FactoryGirl.create :aaro
     user.can_edit_call_results?
   end
 
