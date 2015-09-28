c363d8e2e440609bc50dda8813ec95dcc4ac672a
Petteri Räty <petsku@petteriraty.eu>
2015-09-28 17:08:19 +0300

    Fix rails_admin authentication
    It was letting everyone access it.

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -2,8 +2,16 @@ require 'spec_helper'
 
 describe 'admin', type: :feature do
   it 'should load' do
-    visit '/admins/sign_in'
+    admin = Admin.create! email: 'admin@example.com', password: 'foobar'
 
-    page.should have_content('Log in')
+    visit '/admin'
+
+    fill_in 'Email', with: 'admin@example.com'
+    fill_in 'Password', with: 'foobar'
+
+    click_button 'Log in'
+
+    page.should have_content('Site Administration')
+    page.should have_content('Signed in successfully.')
   end
 end
