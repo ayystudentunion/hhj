21650c230becd7811e922d6b7bc1dbf60abcaf83
Petteri Räty <petsku@petteriraty.eu>
2015-10-01 00:53:53 +0300

    Factory Girl works when running specs individually

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ ENV["RAILS_ENV"] ||= 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'factory_girl'
+require 'factory_girl_rails'
 require 'devise'
 require 'capybara/rspec'
 
