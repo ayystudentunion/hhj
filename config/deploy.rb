5c4b6750fa4b0c2c0e9c4f640e06e52dc9ab6ff0
Petteri Räty <petsku@petteriraty.eu>
2015-10-05 14:45:53 +0300

    Fix the deploy ruby version string
    The production environment is running 2.0 now.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, '2.0.0'        # Or whatever env you want it to run in.
+set :rvm_ruby_string, 'ruby-2.0.0-p645'   # Or whatever env you want it to run in.
 set :rvm_type, :user
 
 require "bundler/capistrano"
